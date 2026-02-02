1d9cef8d91ebaebe57a14f280d7b5ac2af7fb6f6
Aadarsha <kc.aadarsha.ak@gmail.com>
2026-02-02 21:39:41 +0545

    Updated feature

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,7 @@
+<hr>
+        <p style="text-align: center; color: #777;">
+            <small>&copy; <?php echo date('Y'); ?> Visitor Management System (Academic Project by Aadarsha K.C)</small>
+        </p>
 </div>
 <script src="../assets/js/script.js"></script>
 </body>
